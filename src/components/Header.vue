bcda07bf99c873c3c92dafcd9bc82157c855bf9a
zyan <ww9720@gmail.com>
2021-12-22 00:15:54 +0900

    [Feat] 틀 제작 20211222-12:15

---
+++ src/components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-container absolute class="header">
+      <div class="headerBtn">
+        <section v-if="isLogin === true">
+          <div @click="logout">
+            <router-link :to="{name:'IndexMain'}" style="text-decoration:none; color: black; font-weight: bold">로그아웃&nbsp;&nbsp;
+            </router-link>
+          </div>
+          <router-link :to="{name:'editpage'}" style="text-decoration:none; color: black; font-weight: bold">마이페이지
+          </router-link>
+        </section>
+        <section v-else>
+          <router-link :to="{name:'loginform'}" style="text-decoration:none; color: black; font-weight: bold">로그인&nbsp;&nbsp;
+          </router-link>
+          <router-link :to="{name:'register'}" style="text-decoration:none; color: black; font-weight: bold">회원가입
+          </router-link>
+        </section>
+      </div>
+  </v-container>
+</template>
+
+<script>
+import {mapActions, mapState} from 'vuex';
+
+export default {
+  name: "Header",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["isLogin"])
+  },
+  methods : {
+    ...mapActions(["logout"])
+  }
+};
+</script>
+
+<style scoped>
+/* 
+
+#header-container {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  min-width: 1130px;
+  height: 50px;
+  margin: 0 auto;
+  background-color: black;
+}
+
+.headerBtn {
+  float: right;
+  text-align: center;
+  margin: 0 auto;
+  font-size: 100%;
+  background-color: red;
+  text-decoration: none;
+}
+*/
+
+/* .header {
+  float: right;
+  margin: 0px;
+  padding: 0px;
+  color: black;
+} */
+
+/* .logIn {
+  float: right;
+  width: 100%;
+} */
+
+</style>
---
+++ src/views/main/indexMain.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script> 
+
+<style>
+.bawbody {
+    width: 70%;
+    min-height: 800px;
+    /* padding-bottom: 200px; */
+    margin-right: auto;
+    margin-left: auto;
+    color: #44d9e8;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /* justify-content: center; */
+}
+</style>
