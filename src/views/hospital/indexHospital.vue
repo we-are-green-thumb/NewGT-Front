e8e0f76a9e8b9d4946e9a6e3c1d918cb22692c7f
yyh <younghun7513@gmail.com>
2021-12-23 00:52:53 +0900

    [Feat] 식물병원 식물등록 페이지 추가 202112123-00:52

--- src/views/hospital/indexHospital.vue
+++ src/views/hospital/indexHospital.vue
@@ -1,13 +1,21 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <form>
+      <fieldset style="float: left">
+        <legend>식물병원이에요</legend>
+
+        <div class="form-group">
+          <label for="formFile" class="form-label mt-4"
+            >식물을 진단합니다</label
+          >
+          <input class="form-control" type="file" id="formFile" />
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </fieldset>
+    </form>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
-<style>
-    
-</style>
+<style></style>
