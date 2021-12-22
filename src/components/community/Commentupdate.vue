25ba0e86d475f791caf219c1b141ff796128dccb
zyan <ww9720@gmail.com>
2021-12-22 23:47:43 +0900

    [Fix] 파일명 변경 20211222-23:47

---
+++ src/components/community/Commentupdate.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        댓글 수정하기
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
---
+++ src/components/mypage/Myprofile.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        내 정보
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
