0ef050e2a8ed3f66454f29bb80bd3b87110a0f56
yyh <younghun7513@gmail.com>
2021-12-23 19:47:03 +0900

    [Feat]indexplantplus.vue , indexHospital.vue , install jquery 20211223-19:46

--- src/views/plant/indexplantplus.vue
+++ src/views/plant/indexplantplus.vue
@@ -2,155 +2,114 @@
     <div>
         <form>
   <fieldset>
-    <legend>식물을 등록하세요</legend>
+    <legend>🌱식물 등록🌱</legend>
     <div class="form-group row">
-      <label for="staticEmail" class="col-sm-2 col-form-label">별명</label>
+      <label for="staticEmail" class="col-sm-6 col-form-label">식물의 별명을 지어주세요</label>
       <div class="col-sm-10">
-        <input type="text"  class="form-control" id="staticEmail" placeholder="레몬">
+        <input type="text"  class="form-control" id="staticEmail" placeholder="레몬" v-model="nickName">
       </div>
     </div>
-    <div class="form-group">
+    <div class="form-group" style="text-align:left;">
 
-          <label for="formFile" class="form-label mt-4"
-            >식물의 사진을 등록해주세요</label
+          <label for="formFile" class="col-sm-6 col-form-label" >식물의 사진을 등록해주세요</label
           >
       <!-- <label for="staticEmail" class="col-sm-6 col-form-label">마지막으로 물을 언제주셨나요</label> -->
 
-          <input class="form-control" type="file" id="formFile" />
+          <input class="form-control" type="file"  v-bind="fileList" id="input_img" @change="fileChange"/>
         </div>
-    <div class="form-group row">
-      <label for="staticEmail" class="col-sm-4 col-form-label">식물의 종</label>
+    <div class="form-group row"  style="text-align:left;">
+      <label for="staticEmail" class="col-sm-4 col-form-label" >식물의 종</label>
       <div class="col-sm-10">
-        <input type="text"  class="form-control" id="staticEmail" placeholder="레몬">
+        <input type="text"  class="form-control" id="staticEmail" placeholder="선인장" v-model="name">
       </div>
     </div>
 
     <div class="form-group row">
       <label for="staticEmail" class="col-sm-6 col-form-label">마지막으로 물을 언제주셨나요</label>
       <div class="col-sm-10">
-        <input type="text"  class="form-control" id="staticEmail" placeholder="1">
+        <input type="text"  class="form-control" id="staticEmail" placeholder="1" v-model="water">
       </div>
     </div>
-    <div class="form-group row">
+    <div class="form-group row"  style="text-align:left;">
       <label for="staticEmail" class="col-sm-3 col-form-label">온도</label>
       <div class="col-sm-10">
-        <input type="text"  class="form-control" id="staticEmail" placeholder="1">
+        <input type="text"  class="form-control" id="staticEmail" placeholder="1"  v-model="temp" >
       </div>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button @click="registerPlant()" class="btn btn-primary">Submit</button>
   </fieldset>
 </form>
     </div>
 </template>
+
+
 <script>
+import http from "@/util/http-common";
+import $ from "jquery";
+
 export default {
-    
-}
+  name: "Content",
+  data() {
+    return {
+      name: "",
+      nickName: "",
+      water: "",
+      temp: "",
+      imageUrl: "",
+      fileList: [],
+    };
+  },
+  methods: {
+    fileChange() {
+      var file = document.getElementById("input_img");
+      var form = new FormData();
+      form.append("image", file.files[0]);
+
+      var settings = {
+        url: "https://api.imgbb.com/1/upload?key=076f41cee131349132a08f6320271a31",
+        method: "POST",
+        timeout: 0,
+        processData: false,
+        mimeType: "multipart/form-data",
+        contentType: false,
+        data: form,
+      };
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        var jx = JSON.parse(response);
+        this.fileUrl = jx.data.url + "";
+        localStorage.setItem("fileUrl", this.fileUrl);
+      });
+    },
+    registerPlant() {
+      let fileUrl = localStorage.getItem("fileUrl");
+      let userId = localStorage.getItem("getId");
+      let token = localStorage.getItem("getToken");
+      var data = {
+        name: this.name,
+        nickName: this.nickName,
+        water: this.water,
+        temp: this.temp,
+        imageUrl: fileUrl,
+      };
+      http
+        .post("/user/"+userId+"/plant", data, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .then(() => {
+          this.$router.go(this.$router.currentRoute, alert("저장완료"));
+          localStorage.removeItem('fileUrl');
+        });
+    },
+  },
+};
 </script>
 <style>
     
-</style<form>
-  <fieldset>
-    <legend>Legend</legend>
-    <div class="form-group row">
-      <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-      <div class="col-sm-10">
-        <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" value="email@example.com">
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-    <div class="form-group">
-      <label for="exampleSelect1" class="form-label mt-4">Example select</label>
-      <select class="form-select" id="exampleSelect1">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="exampleSelect2" class="form-label mt-4">Example multiple select</label>
-      <select multiple="" class="form-select" id="exampleSelect2">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="exampleTextarea" class="form-label mt-4">Example textarea</label>
-      <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
-    </div>
-    <div class="form-group">
-      <label for="formFile" class="form-label mt-4">Default file input example</label>
-      <input class="form-control" type="file" id="formFile">
-    </div>
-    <fieldset class="form-group">
-      <legend class="mt-4">Radio buttons</legend>
-      <div class="form-check">
-        <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
-          Option one is this and that—be sure to include why it's great
-        </label>
-      </div>
-      <div class="form-check">
-        <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
-          Option two can be something else and selecting it will deselect option one
-        </label>
-      </div>
-      <div class="form-check disabled">
-        <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios3" value="option3" disabled="">
-          Option three is disabled
-        </label>
-      </div>
-    </fieldset>
-    <fieldset class="form-group">
-      <legend class="mt-4">Checkboxes</legend>
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-          Default checkbox
-        </label>
-      </div>
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked="">
-        <label class="form-check-label" for="flexCheckChecked">
-          Checked checkbox
-        </label>
-      </div>
-    </fieldset>
-    <fieldset>
-      <legend class="mt-4">Switches</legend>
-      <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-        <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
-      </div>
-      <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
-        <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
-      </div>
-    </fieldset>
-    <fieldset class="form-group">
-      <legend class="mt-4">Ranges</legend>
-        <label for="customRange1" class="form-label">Example range</label>
-        <input type="range" class="form-range" id="customRange1">
-        <label for="disabledRange" class="form-label">Disabled range</label>
-        <input type="range" class="form-range" id="disabledRange" disabled="">
-        <label for="customRange3" class="form-label">Example range</label>
-        <input type="range" class="form-range" min="0" max="5" step="0.5" id="customRange3">
-    </fieldset>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </fieldset>
-</form>e>
+</style>
