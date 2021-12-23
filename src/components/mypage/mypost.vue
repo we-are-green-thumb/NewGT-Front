92a3b2f959578891b280048df81104c267ee9b76
yyh <younghun7513@gmail.com>
2021-12-24 01:30:06 +0900

    [Add] mypage.vue ... 20211224-01:30

--- src/components/mypage/mypost.vue
+++ src/components/mypage/mypost.vue
@@ -1,12 +1,52 @@
 <template>
-    <div>
-        내 게시글
-    </div>
+<div class="contents">
+     <div>
+    내가 쓴 글 {{posts.length}}
+ 
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col" style="width:60%">제목</th>
+          <th scope="col" >작성자</th>
+          <th scope="col">좋아요</th>
+          <th scope="col">조회수</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post,idx in posts" :key="idx">
+          <th scope="row" class=titletd>&nbsp;&nbsp;{{post.title}}</th>
+          <td>{{ post.writer}}</td>
+          <td>{{ post.like }}</td>
+          <td>{{ post.hits}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
 </template>
 <script>
+import http from "@/util/http-common";
+
 export default {
-    
-}
+  data() {
+    return {
+      posts: [],
+      userID :"",
+    };
+  },
+   created() {
+     this.userID = localStorage.getItem('getId');
+        http
+      .get("/posts/user/"+this.userID) //게시글 불러옴.
+      .then((res) => {
+        this.posts = res.data;
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    },
+};
 </script>
 <style>
     
